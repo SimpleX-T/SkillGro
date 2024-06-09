b8d579e15fc1c735beb72c7f1975eae321695256
Xcodes <ntmark2004@gmail.com>
2024-06-09 20:01:54 +0100

    done for the day

--- src/Components/Course Portal/CourseViewer.jsx
+++ src/Components/Course Portal/CourseViewer.jsx
@@ -111,7 +111,7 @@ function CourseCard({ course }) {
 					</div>
 
 					<div className='flex items-center text-gray-500 text-sm first-letter:border-t-2 border-solid'>
-						{/* continuing here next time */}
+						{/* continuing here tomorrow */}
 						<div className='flex gap-1'>
 							<img
 								src='/Icons/book.png'
