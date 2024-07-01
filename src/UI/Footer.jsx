75b6ebd455046540a980447b3b322880d3628801
Xcodes <ntmark2004@gmail.com>
2024-07-01 14:27:56 +0100

    footer

--- src/UI/Footer.jsx
+++ src/UI/Footer.jsx
@@ -1,5 +1,9 @@
 function Footer() {
-	return <footer className='bg-primary w-full h-[300px]'>hello</footer>;
+	return (
+		<footer className='bg-primary w-full h-[300px]'>
+			<div className='w-5/6'></div>
+		</footer>
+	);
 }
 
 export default Footer;
